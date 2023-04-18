36b462909cd6943055defc2e796db0e566da27ad
J Elliston <jonathan.elliston@gmail.com>
2023-04-18 15:58:15 -0700

    did things

--- src/main/java/icehighway/IceHighwayAddon.java
+++ src/main/java/icehighway/IceHighwayAddon.java
@@ -1,18 +1,21 @@
-package com.example.addon;
+package icehighway;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+// import com.example.addon.commands.CommandExample;
+// import com.example.addon.hud.HudExample;
+// import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
+// import meteordevelopment.meteorclient.systems.commands.Commands;
+// import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+// import meteordevelopment.meteorclient.systems.modules.world.IceHighwayBuilder;
+import icehighway.modules.IceHighwayBuilder;
+
 import org.slf4j.Logger;
 
-public class Addon extends MeteorAddon {
+public class IceHighwayAddon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("IceHighway CATEGORY");
     public static final HudGroup HUD_GROUP = new HudGroup("IceHighway HUD_GROUP");
@@ -22,13 +25,13 @@ public class Addon extends MeteorAddon {
         LOG.info("Initializing Meteor Addon IceHighway");
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new IceHighwayBuilder());
 
         // Commands
-        Commands.get().add(new CommandExample());
+        // Commands.get().add(new CommandExample());
 
         // HUD
-        Hud.get().register(HudExample.INFO);
+        // Hud.get().register(HudExample.INFO);
     }
 
     @Override
